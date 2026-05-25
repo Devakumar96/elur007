9672d37bb3fb3cf2157e51786e9ba73c71f782aa
Devakumar <ndeva96@gmail.com>
2026-05-25 07:37:18 +0530

    fixed git issue

---
+++ js/bus-data.js
@@ -0,0 +1,326 @@
+// ==========================================
+// BUS TIMETABLE DATA
+// ==========================================
+
+const busSchedule = [
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "05:15"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "05:55"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "06:00"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "06:15"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "06:20"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "06:40"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "08:05"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "08:10"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "08:20"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "08:25"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "08:45"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "08:52"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "09:30"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "10:50"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "13:30"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "13:55"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "14:25"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "14:45"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "15:43"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "16:10"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "16:25"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "16:40"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "17:35"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "18:25"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "18:35"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "18:55"
+    }
+
+];
+// ==========================================
+// சத்தி TO கோபி BUS TIMETABLE
+// ==========================================
+
+const சத்திToகோபிSchedule = [
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "05:45"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "06:20"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "06:35"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "06:50"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "07:05"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "07:25"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "08:50"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "09:00"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "09:15"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "09:30"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "09:50"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "10:05"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "10:45"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "12:05"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "14:10"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "14:35"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "15:00"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "15:20"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "16:15"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "16:40"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "17:00"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "17:20"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "18:10"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "18:50"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "19:05"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "19:20"
+    }
+
+];

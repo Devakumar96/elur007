9d518fef02c9f5647832e35e23e47bbc254fb296
Devakumar <ndeva96@gmail.com>
2026-05-31 17:53:19 +0530

    Fix nested repository

---
+++ elur007/js/bus-data.js
@@ -0,0 +1,862 @@
+// ==========================================
+// BUS TIMETABLE DATA
+// ==========================================
+
+const busSchedule = [
+{
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "04:40"
+    },
+    {
+        busNumber: "SSS",
+        route: "ஈரோடு → சத்தி",
+        time: "05:00"
+    },
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "05:05"
+    },
+
+    {
+        busNumber: "JK",
+        route: "கோபி → சத்தி",
+        time: "05:20"
+    },
+
+    {
+        busNumber: "KALAIMAGAL",
+        route: "ஈரோடு → சத்தி",
+        time: "05:25"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "05:30"
+    },
+
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "05:45"
+    },
+
+    {
+        busNumber: "PMS",
+        route: "ஈரோடு → சத்தி",
+        time: "05:55"
+    },
+
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "05:57"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "06:00"
+    },
+
+    {
+        busNumber: "SSS",
+        route: "ஈரோடு → சத்தி",
+        time: "06:30"
+    },
+
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "06:40"
+    },
+
+    {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "06:47"
+    },
+
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "06:50"
+    },
+
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "06:57"
+    },
+
+    {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "07:00"
+    },
+
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "07:10"
+    },
+
+    {
+        busNumber: "NANDHI",
+        route: "கோபி → சத்தி",
+        time: "07:15"
+    },
+
+    {
+        busNumber: "KANGA",
+        route: "கோபி → சத்தி",
+        time: "07:20"
+    },
+
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "07:30"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "07:32"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "07:45"
+    },
+    {
+        busNumber: "RAJAM",
+        route: "கோபி → சத்தி",
+        time: "07:47"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "07:50"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "07:55"
+    },
+
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "08:15"
+    },
+    {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "08:17"
+    },
+
+    {
+        busNumber: "TNSTC",
+        route: "கோபி → KNபாளையம்",
+        time: "08:20"
+    },
+    {
+        busNumber: "11",
+        route: "அந்தியூர் → சத்தி",
+        time: "08:25"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "08:35"
+    },
+
+    {
+        busNumber: "SSS",
+        route: "ஈரோடு → சத்தி",
+        time: "08:40"
+    },
+
+    {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "08:55"
+    },
+
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "09:00"
+    },
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "09:05"
+    },
+    {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "09:20"
+    },
+    {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "09:30"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "09:32"
+    },
+    {
+        busNumber: "PMS",
+        route: "ஈரோடு → சத்தி",
+        time: "09:40"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "09:50"
+    },
+    {
+        busNumber: "NANDHI",
+        route: "கோபி → சத்தி",
+        time: "09:55"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "09:57"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "10:10"
+    },
+    {
+        busNumber: "RAJAM",
+        route: "கோபி → சத்தி",
+        time: "10:15"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "10:17"
+    },
+    {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "10:25"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "10:30"
+    },
+    {
+        busNumber: "ST",
+        route: "அந்தியூர் → சத்தி",
+        time: "10:45"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "10:47"
+    },
+    {
+        busNumber: "PMS",
+        route: "ஈரோடு → சத்தி",
+        time: "11:00"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "11:05"
+    },
+    {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "11:20"
+    },
+
+    {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "11:22"
+    },
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "11:25"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "11:50"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "11:55"
+    },
+    {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "12:00"
+    },
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "12:10"
+    }, 
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "12:15"
+    },
+     {
+        busNumber: "NANDHI",
+        route: "கோபி → சத்தி",
+        time: "12:30"
+    },
+     {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "12:40"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "12:45"
+    }, 
+    {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "12:50"
+    },
+     {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "13:00"
+    },
+     {
+        busNumber: "RAJAM",
+        route: "கோபி → சத்தி",
+        time: "13:05"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "13:07"
+    }, 
+    {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "13:15"
+    },
+     {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "13:20"
+    },
+     {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "13:25"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "13:45"
+    }, 
+    {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "13:47"
+    },
+     {
+        busNumber: "KANGA",
+        route: "கோபி → சத்தி",
+        time: "13:50"
+    },
+     {
+        busNumber: "PMS",
+        route: "ஈரோடு → சத்தி",
+        time: "13:55"
+    },
+     {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "13:57"
+    },
+     {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "14:00"
+    },
+     {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "14:05"
+    },
+     {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "14:15"
+    },
+     {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "14:20"
+    },
+     {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "14:45"
+    },
+     {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "14:50"
+    },
+     {
+        busNumber: "SBT",
+        route: "ஈரோடு → சத்தி",
+        time: "14:55"
+    }, {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "15:05"
+    }, {
+        busNumber: "NANDTHI",
+        route: "கோபி → சத்தி",
+        time: "15:07"
+    }, {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "15:15"
+    }, {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "15:20"
+    }, {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "15:35"
+    }, {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "15:37"
+    }, {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "15:50"
+    }, {
+        busNumber: "ST",
+        route: "ஈரோடு → சத்தி",
+        time: "15:55"
+    }, {
+        busNumber: "RAJAM",
+        route: "கோபி → சத்தி",
+        time: "16:00"
+    }, {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "16:05"
+    }, {
+        busNumber: "JK",
+        route: "கோபி → சத்தி",
+        time: "16:20"
+    }, {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "16:25"
+    }, {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "16:35"
+    }, {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "16:45"
+    }, {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "17:00"
+    }, {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "17:05"
+    }, {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "17:20"
+    }, {
+        busNumber: "NANDHI",
+        route: "கோபி → சத்தி",
+        time: "17:25"
+    }, {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "17:45"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "17:47"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "17:55"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "18:00"
+    },
+    {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "18:02"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "18:15"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "18:17"
+    },
+    {
+        busNumber: "VBS",
+        route: "ஈரோடு → சத்தி",
+        time: "18:20"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "18:35"
+    },
+    {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "18:40"
+    },
+    {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "18:45"
+    },
+    {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "18:46"
+    },
+    {
+        busNumber: "PMS",
+        route: "ஈரோடு → சத்தி",
+        time: "18:47"
+    },
+    {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "19:00"
+    },
+    {
+        busNumber: "RAJAM",
+        route: "கோபி → சத்தி",
+        time: "19:10"
+    },
+    {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "19:20"
+    },
+    {
+        busNumber: "ST",
+        route: "அந்தியூர் → சத்தி",
+        time: "19:25"
+    },
+    {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "19:30"
+    },
+    {
+        busNumber: "SSS",
+        route: "ஈரோடு → சத்தி",
+        time: "19:40"
+    },
+     {
+        busNumber: "NANDHI",
+        route: "கோபி → சத்தி",
+        time: "19:45"
+    },
+     {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "19:55"
+    },
+     {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "20:15"
+    },
+     {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "20:20"
+    },
+     {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "20:30"
+    },
+     {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "20:35"
+    },
+     {
+        busNumber: "CBS",
+        route: "கோபி → சத்தி",
+        time: "20:50"
+    },
+
+     {
+        busNumber: "PMS",
+        route: "ஈரோடு → சத்தி",
+        time: "20:55"
+    },
+
+     {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "21:00"
+    },
+
+     {
+        busNumber: "TNSTC",
+        route: "அந்தியூர் → சத்தி",
+        time: "21:10"
+    },
+
+     {
+        busNumber: "3B",
+        route: "கோபி → சத்தி",
+        time: "21:25"
+    },
+
+     {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "21:35"
+    },
+
+     {
+        busNumber: "3C",
+        route: "கோபி → சத்தி",
+        time: "21:45"
+    },
+
+     {
+        busNumber: "4",
+        route: "அத்தாணி → சத்தி",
+        time: "21:55"
+    },
+
+     {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "21:57"
+    },
+
+     {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "22:20"
+    },
+
+     {
+        busNumber: "3A",
+        route: "கோபி → KNபாளையம்",
+        time: "23:05"
+    },
+     {
+        busNumber: "11",
+        route: "அத்தாணி → சத்தி",
+        time: "23:20"
+    },
+
+
+];
+// ==========================================
+// சத்தி TO கோபி BUS TIMETABLE
+// ==========================================
+
+const சத்திToகோபிSchedule = [
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "05:45"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "06:20"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "06:35"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "06:50"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "07:05"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "07:25"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "08:50"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "09:00"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "09:15"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "09:30"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "09:50"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "10:05"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "10:45"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "12:05"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "14:10"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "14:35"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "15:00"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "15:20"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "16:15"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "16:40"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "17:00"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "17:20"
+    },
+
+    {
+        busNumber: "3C",
+        route: "சத்தி → கோபி",
+        time: "18:10"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "18:50"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "19:05"
+    },
+
+    {
+        busNumber: "3B",
+        route: "சத்தி → கோபி",
+        time: "19:20"
+    }
+
+];
---
+++ elur007/js/script.js
@@ -0,0 +1,283 @@
+
+// ==========================================
+// COPY PHONE NUMBER
+// ==========================================
+
+function copyNumber(number) {
+
+    navigator.clipboard.writeText(number);
+
+    alert(number + " copied");
+
+}
+// ==========================================
+// GET ALL CARDS
+// ==========================================
+
+const cards =
+    document.querySelectorAll(".bus-card");
+
+
+// ==========================================
+// FIND NEXT BUS
+// ==========================================
+document.addEventListener("DOMContentLoaded", function(){
+
+    const toggleBtn = document.getElementById("toggleBtn");
+    const dots = document.getElementById("dots");
+    const moreText = document.getElementById("moreText");
+
+    toggleBtn.addEventListener("click", function(){
+
+        if(moreText.style.display === "none"){
+
+            moreText.style.display = "inline";
+            dots.style.display = "none";
+
+            toggleBtn.innerHTML =
+            '<i class="fas fa-angle-up"></i>';
+
+        }else{
+
+            moreText.style.display = "none";
+            dots.style.display = "inline";
+
+            toggleBtn.innerHTML =
+            '<i class="fas fa-angle-down"></i>';
+
+        }
+
+    });
+
+});
+
+function getNextBus(schedule) {
+
+    const now =
+        new Date();
+
+    let nextBus = null;
+
+    for (let bus of schedule) {
+
+        const [hours, minutes] =
+            bus.time.split(":");
+
+        const busTime =
+            new Date();
+
+        // Today's Bus Time
+        busTime.setHours(hours);
+        busTime.setMinutes(minutes);
+        busTime.setSeconds(0);
+
+        // Future Bus Found
+        if (busTime > now) {
+
+            nextBus = {
+
+                ...bus,
+
+                busTime
+
+            };
+
+            break;
+
+        }
+
+    }
+
+
+    // ==========================================
+    // IF NO BUS LEFT TODAY
+    // TAKE FIRST BUS OF NEXT DAY
+    // ==========================================
+
+    if (!nextBus) {
+
+        const firstBus =
+            schedule[0];
+
+        const [hours, minutes] =
+            firstBus.time.split(":");
+
+        const tomorrowBusTime =
+            new Date();
+
+        // Add Next Day
+        tomorrowBusTime.setDate(
+            tomorrowBusTime.getDate() + 1
+        );
+
+        tomorrowBusTime.setHours(hours);
+        tomorrowBusTime.setMinutes(minutes);
+        tomorrowBusTime.setSeconds(0);
+
+        nextBus = {
+
+            ...firstBus,
+
+            busTime: tomorrowBusTime
+
+        };
+
+    }
+
+    return nextBus;
+
+}
+
+
+// ==========================================
+// UPDATE SINGLE CARD
+// ==========================================
+
+function updateCard(card, schedule) {
+
+    const nextBus =
+        getNextBus(schedule);
+
+    const busNumber =
+        card.querySelector(".bus-number");
+
+    const routeText =
+        card.querySelector(".route-text");
+
+    const countdown =
+        card.querySelector(".countdown");
+
+
+  
+
+    // Update Bus Details
+
+    busNumber.innerText =
+        nextBus.busNumber;
+
+    routeText.innerText =
+        nextBus.route;
+
+
+    // Time Difference
+
+    const now =
+        new Date().getTime();
+
+    const distance =
+        nextBus.busTime.getTime() - now;
+
+
+    // Hours
+
+    const hours =
+        Math.floor(
+            distance / (1000 * 60 * 60)
+        );
+
+    // Minutes
+
+    const minutes =
+        Math.floor(
+            (distance % (1000 * 60 * 60))
+            / (1000 * 60)
+        );
+
+    // Seconds
+
+    const seconds =
+        Math.floor(
+            (distance % (1000 * 60))
+            / 1000
+        );
+
+
+    // Display
+
+    countdown.innerText =
+        `${hours}h ${minutes}m ${seconds}s`;
+
+}
+
+
+// ==========================================
+// LIVE UPDATE
+// ==========================================
+
+function startLiveCountdown() {
+
+    // Card 1 → Gobi to Sathy
+
+    updateCard(
+        cards[0],
+        busSchedule
+    );
+
+    // Card 2 → Sathy to Gobi
+
+    updateCard(
+        cards[1],
+        சத்திToகோபிSchedule
+    );
+
+}
+
+
+// Initial Load
+
+startLiveCountdown();
+
+
+// Update Every Second
+
+setInterval(() => {
+
+    startLiveCountdown();
+
+}, 1000);
+
+// ==========================================
+// THEME TOGGLER
+// ==========================================
+
+const themeToggler =
+    document.querySelector(".theme-toggler");
+
+const toggleBtn =
+    document.querySelector(".toggle-btn");
+
+
+// Open / Close
+
+toggleBtn.onclick = () => {
+
+    themeToggler.classList.toggle("active");
+
+};
+
+
+// ==========================================
+// COLOR CHANGE
+// ==========================================
+
+const themeButtons =
+    document.querySelectorAll(".theme-btn");
+
+
+themeButtons.forEach(button => {
+
+    button.onclick = () => {
+
+        const color =
+            button.getAttribute("data-color");
+
+        // Change Main Theme Color
+
+        document.documentElement.style.setProperty(
+            "--pumpkin-spice",
+            color
+        );
+
+    };
+
+});
